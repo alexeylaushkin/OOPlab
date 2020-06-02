b9881f1c278ef4649538f95ea451bed83a80bc8e
alexeylaushkin <iodov@yandex.ru>
2020-06-02 11:11:38 +0300

    4
    4

---
+++ KLADobj.WebService/InfrastructureServices/Gateways/Database/KLADobjContext.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using KLADobj.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KLADobj.InfrastructureServices.Gateways.Database
+{
+    public class KLADobjContext : DbContext
+    {
+        public DbSet<kladobj> KLADobjs { get; set; }
+
+        public KLADobjContext(DbContextOptions options)
+            : base(options)
+        { }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            FillTestData(modelBuilder);
+        }
+        private void FillTestData(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<kladobj>().HasData(
+                new
+                {
+                    Id = 1L,
+                    Name = "Стационарный общественный туалет № 123",
+                    Address = "город Москва, Манежная улица, дом 2-10, сооружение 1",
+                    ChillType = "бесплатный",
+                    Free = "действует",
+                    
+                },
+                new
+                {
+                    Id = 2L,
+                    Name = "Стационарный общественный туалет № 45",
+                    Address = "город Москва, Большая Садовая улица, дом 16, сооружение 1",
+                    ChillType = "бесплатный",
+                    Free = "действует",
+
+                },
+                new
+                {
+                    Id = 3L,
+                    Name = "Стационарный общественный туалет № 13",
+                    Address = "город Москва, улица Земляной Вал, дом 1/4, строение 1",
+                    ChillType = "бесплатный",
+                    Free = "действует",
+
+                },
+                new
+                {
+                    Id = 4L,
+                    Name = "Стационарный общественный туалет № 85",
+                    Address = "город Москва, Марксистская улица, дом 2, строение 2",
+                    ChillType = "бесплатный",
+                    Free = "действует",
+                }
+               
+            );
+        }
+    }
+}
---
+++ KLADobj.WebService/InfrastructureServices/Gateways/Database/KLADobjEFSqliteGateway.cs
@@ -0,0 +1,47 @@
+﻿using KLADobj.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Linq.Expressions;
+using KLADobj.ApplicationServices.Ports.Gateways.Database;
+
+namespace KLADobj.InfrastructureServices.Gateways.Database
+{
+    public class KLADobjEFSqliteGateway : IKLADobjDatabaseGateway
+    {
+        private readonly KLADobjContext _kladobjContext;
+
+        public KLADobjEFSqliteGateway(KLADobjContext KLADobjContext)
+            => _kladobjContext = KLADobjContext;
+
+        public async Task<kladobj> GetKLADobj(long id)
+           => await _kladobjContext.KLADobjs.Where(b => b.Id == id).FirstOrDefaultAsync();
+
+        public async Task<IEnumerable<kladobj>> GetAllKLADobjs()
+            => await _kladobjContext.KLADobjs.ToListAsync();
+          
+        public async Task<IEnumerable<kladobj>> QueryKLADobjs(Expression<Func<kladobj, bool>> filter)
+            => await _kladobjContext.KLADobjs.Where(filter).ToListAsync();
+
+        public async Task AddKLADobj(kladobj kladobj)
+        {
+            _kladobjContext.KLADobjs.Add(kladobj);
+            await _kladobjContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateKLADobj(kladobj kladobj)
+        {
+            _kladobjContext.Entry(kladobj).State = EntityState.Modified;
+            await _kladobjContext.SaveChangesAsync();
+        }
+
+        public async Task RemoveKLADobj(kladobj kladobj)
+        {
+            _kladobjContext.KLADobjs.Remove(kladobj);
+            await _kladobjContext.SaveChangesAsync();
+        }
+
+    }
+}
